9e0fb5fd9154a7ba96382156f12b99798f6e3d0a
Sathiya R <rskeerthi01@gmail.com>
2026-07-02 23:22:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counters = 0;
+
+function increment() {
+    alert(counters);
+    counters++;
+    document.getElementById("counter").innerText = counters;
+}
